56507b3a4cfd3b00c0596b44d33da181522fcfab
RutgerMB <135663615+RutgerMB@users.noreply.github.com>
2026-07-09 21:52:29 +0200

    Update Package.swift

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -11,6 +11,8 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
+        .package(name: "RepLockControls", path: "../LocalPackages/RepLockControls"),
+        .package(name: "CapgoNativePurchases", path: "../LocalPackages/CapgoNativePurchases"),
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.1")
     ],
     targets: [
@@ -18,7 +20,9 @@ let package = Package(
             name: "CapApp-SPM",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "RepLockControls", package: "RepLockControls"),
+                .product(name: "CapgoNativePurchases", package: "CapgoNativePurchases")
             ]
         )
     ]
